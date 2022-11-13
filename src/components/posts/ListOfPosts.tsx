acd0815c1ac5742355e8cece808b6a670e04359a
Satyra1 <platek549@gmail.com>
2022-11-13 10:50:56 +0100

    .

--- src/components/posts/ListOfPosts.tsx
+++ src/components/posts/ListOfPosts.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { IPost } from "../../redux/api/types";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { setPostLikes } from "../../redux/slices/Posts/Posts.slice";
+import SearchUser from "../ui/LoggedIn/SearchUser";
 import SinglePost from "./SinglePost";
 import { AllPostContainer } from "./SinglePost.styled";
 
@@ -9,7 +10,6 @@ const ListOfPosts = ({ posts }: { posts: IPost[] }) => {
   const dispatch = useAppDispatch();
   const { UserQueryId } = useAppSelector((state) => state.Posts);
   const [postToDisplay, setPostToDisplay] = useState(posts);
-
   useEffect(() => {
     const postLikes = posts.map((post) => {
       return { postId: post.id, numberOfLikes: Math.random() * 1000 };
@@ -28,6 +28,7 @@ const ListOfPosts = ({ posts }: { posts: IPost[] }) => {
 
   return (
     <AllPostContainer>
+      <SearchUser />
       {postToDisplay.map((post) => {
         return <SinglePost key={post.id} post={post} />;
       })}
